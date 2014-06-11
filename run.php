aa8f3cedf87d9d3d80a01efb18ecf8d8a82bbeb8
Gregwar <g.passault@gmail.com>
2014-06-11 11:42:17 +0200

    Fixes in run

--- run.php
+++ run.php
@@ -5,17 +5,18 @@ $dir = opendir('.');
 $dimensions = array(
     array(280, 150),
     array(210, 210),
+    array(200, 150),
     array(150, 150)
 );
 
 while ($file = readdir($dir)) {
-    if (is_dir($file) && $file!='.' && $file!='..') {
+    if (is_dir($file) && $file[0]!='.') {
         echo "* Processing $file...\n";
         foreach ($dimensions as $dimension) {
             list($w, $h) = $dimension;
             $t = microtime(true);
             `rm $file/plate_*.stl 2> /dev/null`;
-            `plater -t 4 -W $w -H $h -s 2 $file/plater.conf 2> /dev/null`;
+            `plater -t 4 -W $w -H $h -s 1.75 $file/plater.conf 2> /dev/null`;
             $plates = count(explode("\n",`ls $file/plate_*.stl`))-1;
             $e = microtime(true)-$t;
             echo "~> Width: $w, Height: $h, Time: $e, Plates: $plates\n";
